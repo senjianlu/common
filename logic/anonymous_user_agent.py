82b35ecbfb4a2ed29f9a4645fe16df7ab65b308f
senjianlu <admin@cs.cheap>
2024-03-25 19:34:58 +0800

    fix: 修复 anonymous_user_agent.py 的 SQL 执行错误

--- logic/anonymous_user_agent.py
+++ logic/anonymous_user_agent.py
@@ -10,6 +10,7 @@
 
 
 import random
+from sqlalchemy import text
 
 from common.logger import LOGGER
 from common.Base import init_db
@@ -36,13 +37,13 @@ def init_user_agents():
         """
     try:
         # 2.1 查询
-        result = session.execute(sql).fetchall()
+        result = session.execute(text(sql)).fetchall()
         # 2.2 遍历结果并插入到全局变量中
         global USER_AGENTS
         USER_AGENTS = [item[0] for item in result]
-        LOGGER.info("共通 Anonymous User Agent -> 初始化 User Agents 成功")
+        LOGGER.info("共通 Anonymous User Agent -> 初始化 User Agents 成功，共有 {} 条 User Agents".format(len(USER_AGENTS)))
     except Exception as e:
-        LOGGER.error("共通 Anonymous User Agent -> 初始化 User Agents 失败")
+        LOGGER.error("共通 Anonymous User Agent -> 初始化 User Agents 出错！错误信息：{}".format(e))
     finally:
         session.close()
     return True
