a276c3697476506031fde3e9b764726be9d3b464
senjianlu <admin@cs.cheap>
2024-04-09 20:12:02 +0800

    update: 更新 common.rpc 模块对传入参数类的判断，以少量提升性能

--- rpc.py
+++ rpc.py
@@ -18,21 +18,21 @@ def convert_decimal_to_marked_str(obj):
     :param obj: 待转换对象
     :return: 转换后对象
     """
-    # 1. 如果是字典
-    if isinstance(obj, dict):
+    # 1. 如果是 Decimal
+    if isinstance(obj, Decimal):
+        obj = "<r_c><D>{}</D></r_c>".format(obj)
+    # 2. 如果是字典
+    elif isinstance(obj, dict):
         for key, value in obj.items():
             obj[key] = convert_decimal_to_marked_str(value)
-    # 2. 如果是列表
+    # 3. 如果是列表
     elif isinstance(obj, list):
         for index, value in enumerate(obj):
             obj[index] = convert_decimal_to_marked_str(value)
-    # 3. 如果是自定义对象
+    # 4. 如果是自定义对象
     elif hasattr(obj, "__dict__"):
         for key, value in obj.__dict__.items():
             obj.__dict__[key] = convert_decimal_to_marked_str(value)
-    # 4. 如果是 Decimal
-    elif isinstance(obj, Decimal):
-        obj = "<r_c><D>{}</D></r_c>".format(obj)
     # 5. 返回结果
     return obj
 
@@ -42,20 +42,20 @@ def convert_marked_str_to_decimal(obj):
     :param obj: 待转换对象
     :return: 转换后对象
     """
-    # 1. 如果是字典
-    if isinstance(obj, dict):
+    # 1. 如果是被标记的字符串
+    if isinstance(obj, str) and obj.startswith("<r_c><D>") and obj.endswith("</D></r_c>"):
+        obj = Decimal(obj.replace("<r_c><D>", "").replace("</D></r_c>", ""))
+    # 2. 如果是字典
+    elif isinstance(obj, dict):
         for key, value in obj.items():
             obj[key] = convert_marked_str_to_decimal(value)
-    # 2. 如果是列表
+    # 3. 如果是列表
     elif isinstance(obj, list):
         for index, value in enumerate(obj):
             obj[index] = convert_marked_str_to_decimal(value)
-    # 3. 如果是自定义对象
+    # 4. 如果是自定义对象
     elif hasattr(obj, "__dict__"):
         for key, value in obj.__dict__.items():
             obj.__dict__[key] = convert_marked_str_to_decimal(value)
-    # 4. 如果是被标记的字符串
-    elif isinstance(obj, str) and obj.startswith("<r_c><D>") and obj.endswith("</D></r_c>"):
-        obj = Decimal(obj.replace("<r_c><D>", "").replace("</D></r_c>", ""))
     # 5. 返回结果
     return obj
